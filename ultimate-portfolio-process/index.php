b6339743e565bcbda274a120954cbf02db766d42
Marco R <mprizzuto88@gmail.com>
2023-01-29 18:34:10 -0500

    update index.php for ultimate portfolio framework

--- ultimate-portfolio-process/index.php
+++ ultimate-portfolio-process/index.php
@@ -1,1 +1,11 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ultimate portfolio framework</title>
+</head>
+<body>
+	<h1>ultimate portfolio process homepage</h1>
+</body>
+</html>
