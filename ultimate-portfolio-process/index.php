30554122a46d4f6215646c35db65ceceec55b079
Marco R <mprizzuto88@gmail.com>
2023-01-29 18:54:43 -0500

    Create images folder, add local media

--- ultimate-portfolio-process/index.php
+++ ultimate-portfolio-process/index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="icon" type="image/x-icon" href="./images/star.ico">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>ultimate portfolio framework</title>
+	<meta name="description" content="this is a description. it should be dynamic, and reflect the current page">
+
+	<meta property="og:image" content="./images/jung.png">
 </head>
 <body>
 	<h1>ultimate portfolio process homepage</h1>
