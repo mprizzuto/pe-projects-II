63730f7498e2b2f429434565dacfe0ca32d8dbbf
Marco R <mprizzuto88@gmail.com>
2023-01-29 23:20:41 -0500

    Update file paths fo links, remove html  nav.php

--- ultimate-portfolio-process/components/modules/nav.php
+++ ultimate-portfolio-process/components/modules/nav.php
@@ -1,18 +1,16 @@
-<header class="global-header">
-	<nav class="global-nav">
-		<ul class="page-list">
-			<li>
-				<a href="index.php">home</a>
-			</li>
+<nav class="global-nav">
+	<ul class="page-list">
+		<li>
+			<a href="#">home</a>
+		</li>
 
-			<li>
-				<a href="about.php">about</a>
-			</li>
+		<li>
+			<a href="./components/pages/about.php">about</a>
+		</li>
 
-			<li>
-				<a href="contact.php">contact</a>
-			</li>
+		<li>
+			<a href="./components/pages/contact.php">contact</a>
+		</li>
 
-		</ul>
-	</nav>
-</header>
+	</ul>
+</nav>
