1b6d995a3b889888740415361a821bd190d6c444
Marco R <mprizzuto88@gmail.com>
2023-01-29 19:49:53 -0500

    Create global header/ nav

--- ultimate-portfolio-process/index.php
+++ ultimate-portfolio-process/index.php
@@ -13,6 +13,24 @@
 </head>
 
 <body>
+	<header class="global-header">
+		<nav class="global-nav">
+			<ul class="page-list">
+				<li>
+					<a href="index.php">home</a>
+				</li>
+
+				<li>
+					<a href="about.php">about</a>
+				</li>
+
+				<li>
+					<a href="contact.php">contact</a>
+				</li>
+
+			</ul>
+		</nav>
+	</header>
 	<h1>ultimate portfolio process homepage</h1>
 	
 </body>
