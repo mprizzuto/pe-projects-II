74b125ec03c6b36c408759e1637eaf409aac8862
Marco R <mprizzuto88@gmail.com>
2023-01-31 03:05:33 -0500

    format index

--- ultimate-portfolio-process/index.php
+++ ultimate-portfolio-process/index.php
@@ -1,11 +1,6 @@
 <?php include "functions.php"; ?>
 <?php include "./components/modules/header.php"; ?>
 
-<?php 
-echo getPage() . "get is" . var_dump($_GET);
-// echo is404();
-?>
-
 <section class="<?= sanitizeString(getPage()) ?? "home" ?>-section">
 	<inner-column>
 		<?php generatePage(); ?>
