021a01a2636a94d2ed92371c2578b315ff1ddbb1
Marco R <mprizzuto88@gmail.com>
2023-01-31 00:49:33 -0500

    Create graphic-diptych component add content

---
+++ ultimate-portfolio-process/components/modules/graphic-diptych.php
@@ -0,0 +1,10 @@
+<graphic-diptych>
+	<picture class="diptych-photo">
+		<img src="https://peprojects.dev/images/portrait.jpg">
+	</picture>
+
+	<section class="diptych-section">
+
+		<p>graphic-diptych.php</p>
+	</section>
+</graphic-diptych>
