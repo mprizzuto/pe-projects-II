510d76ac3c7a448a5237105b2b8e95e1228b56a2
Marco R <mprizzuto88@gmail.com>
2023-01-31 03:04:08 -0500

    format and update index.php,call utility functions

--- ultimate-portfolio-process/index.php
+++ ultimate-portfolio-process/index.php
@@ -1,10 +1,17 @@
+<?php include "functions.php"; ?>
+<?php include "./components/modules/header.php"; ?>
+
 <?php 
-include "functions.php";
-include "./components/modules/header.php";
-// echo getPage();
+echo getPage() . "get is" . var_dump($_GET);
 // echo is404();
-generatePage();
-include "./components/modules/footer.php";
-$test = "null";
 ?>
+
+<section class="<?= sanitizeString(getPage()) ?? "home" ?>-section">
+	<inner-column>
+		<?php generatePage(); ?>
+	</inner-column>
+</section>
+
+
+<?php include "./components/modules/footer.php"; ?> 
 
