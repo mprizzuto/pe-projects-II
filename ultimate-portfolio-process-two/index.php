98cebeb904c45ad3a1bf903e8186e9373e42e393
Marco R <mprizzuto88@gmail.com>
2023-02-02 22:27:56 -0500

    Replace index.html with index.php

--- ultimate-portfolio-process-two/index.php
+++ ultimate-portfolio-process-two/index.php
@@ -8,7 +8,7 @@
 	</head>
 	<body>
 		<header class="site-header">
-			<a href="index.html">
+			<a href="index.php">
 				<svg class="site-logo" width="100%" height="100%" viewBox="0 0 1460 900" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
 		    <g transform="matrix(1,0,0,1,-1138.46,-4820.22)">
 		        <g id="asynchronous" transform="matrix(1,0,0,1.125,158.458,4797.72)">
