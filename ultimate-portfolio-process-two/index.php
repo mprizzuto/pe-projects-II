577f978af40bae104e5ce854ae099e734f1d6a48
Marco R <mprizzuto88@gmail.com>
2023-02-02 21:14:51 -0500

    create index.php

--- ultimate-portfolio-process-two/index.php
+++ ultimate-portfolio-process-two/index.php
@@ -1,1 +1,12 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>ultimate portfolio framework</title>
+		<link rel="stylesheet" type="text/css" href="site.css">
+	</head>
+	<body>
+		<h1>index.php</h1>
+	</body>
+</html>
