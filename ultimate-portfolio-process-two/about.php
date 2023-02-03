1953e2581c960851db3b7b9d91bccb6c3cbe100b
Marco R <mprizzuto88@gmail.com>
2023-02-02 23:20:22 -0500

    rename files to .php, incude header and footer

---
+++ ultimate-portfolio-process-two/about.php
@@ -0,0 +1,10 @@
+<?php include "header.php"; ?>
+
+<header class="page-header">
+  <h1>about</h1>
+
+
+  <p>welcome to about page</p>
+</header>
+
+<?php include "footer.php"; ?>
---
+++ ultimate-portfolio-process-two/contact.php
@@ -0,0 +1,16 @@
+<?php include "header.php"; ?>
+<header class="page-header">
+	<h2>contact me</h2>
+
+  <p>welcome to the ultimate portfolio framework index!</p>
+</header>
+
+<section>
+	<h2>other ways to contact me</h2>
+
+	<ul>
+		<li>email</li>
+		<li>social media</li>
+	</ul>
+</section>
+<?php include "footer.php"; ?>
