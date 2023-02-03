51b983da7022cde925cfd4c3c4955510deb997aa
Marco R <mprizzuto88@gmail.com>
2023-02-02 23:23:56 -0500

    Replace .html with .php

--- ultimate-portfolio-process-two/header.php
+++ ultimate-portfolio-process-two/header.php
@@ -49,11 +49,11 @@
 					</li>
 
 					<li>
-						<a href="about.html">About</a>
+						<a href="about.php">About</a>
 					</li>
 
 					<li>
-						<a href="contact.html">Contact</a>
+						<a href="contact.php">Contact</a>
 					</li>
 				</ul>
 			</nav>
