c27fb8487026a65d21a2ca845d072610364884ab
Marco R <mprizzuto88@gmail.com>
2023-02-05 20:35:50 -0500

    Add inner-column to about.php

--- ultimate-portfolio-process-two/about.php
+++ ultimate-portfolio-process-two/about.php
@@ -1,10 +1,18 @@
 <?php include "header.php"; ?>
 
 <header class="page-header">
-  <h1>about</h1>
+  <inner-column>
+  	<h1>about</h1>
 
 
-  <p>welcome to about page</p>
+    <p>welcome to about page</p>
+  </inner-column>
 </header>
+
+<section>
+	<inner-column>
+		<?php include "graphic-diptych.php"; ?>
+	</inner-column>
+</section>
 
 <?php include "footer.php"; ?>
