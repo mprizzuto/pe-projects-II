af3ae6bb7ca8bda0df7548c710e65b2b1069a697
Marco R <mprizzuto88@gmail.com>
2023-02-06 00:19:51 -0500

    Create generic-text.php, add content

---
+++ ultimate-portfolio-process-two/generic-text.php
@@ -0,0 +1,16 @@
+<?php 
+	$content = $content ?? <<< HEREDOC
+		<h2>DEFAULT generic text heading</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+	HEREDOC;
+?>
+
+<generic-text>
+
+</generic-text>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
