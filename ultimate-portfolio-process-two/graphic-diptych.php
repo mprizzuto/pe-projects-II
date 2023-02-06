bf365238deb32fec6db344e25a775992eadd62b4
Marco R <mprizzuto88@gmail.com>
2023-02-05 21:22:06 -0500

    Add h2 to graphic-diptych

--- ultimate-portfolio-process-two/graphic-diptych.php
+++ ultimate-portfolio-process-two/graphic-diptych.php
@@ -4,6 +4,8 @@
 		</div>
 
 		<div class="content">
+			<h2>heading level 2</h2>
+			
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
