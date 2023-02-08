4132fb9bb7bd7aee880d3d099b2600b038600460
Marco R <mprizzuto88@gmail.com>
2023-02-07 20:28:26 -0500

    Format pages

--- ultimate-portfolio-process-two/contact.php
+++ ultimate-portfolio-process-two/contact.php
@@ -5,12 +5,5 @@ $pageTitle = "contact page title";
 include "page-header.php"; 
 ?>
 
-<section>
-	<h2>other ways to contact me</h2>
-
-	<ul>
-		<li>email</li>
-		<li>social media</li>
-	</ul>
-</section>
+<?php include "generic-text.php"; ?>
 <?php include "footer.php"; ?>
--- ultimate-portfolio-process-two/generic-text.php
+++ ultimate-portfolio-process-two/generic-text.php
@@ -8,9 +8,8 @@
 ?>
 
 <generic-text>
-
+	<inner-column>
+		<?=$content?>
+	</inner-column>
 </generic-text>
 
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
