02947d46144ba0b2d4e75253b7271de310e00a65
Marco R <mprizzuto88@gmail.com>
2023-02-13 00:02:23 -0500

    Create home.php, add header to files

--- dynamic-portfolio-adventure/index.php
+++ dynamic-portfolio-adventure/index.php
@@ -5,16 +5,6 @@ index.php
 $page = currentPage();
 ?>
 
-<header>
-	<p><?=queryString();?></p>
-	<h1><?=$page["title"]?></h1>
-	<p><?=$page["intro"]?></p>
-
-	<?php include "partials/site-map.php"; ?>
-</header>
+<p><?=queryString();?></p>
 
 <?php renderPageTemplate(currentPage()); ?>
-
-<section>
-	<h2>section heading</h2>
-</section>
---
+++ dynamic-portfolio-adventure/pages/home.php
@@ -0,0 +1,6 @@
+<header>
+	<h1><?=$page["title"]?></h1>
+	<p><?=$page["intro"]?></p>
+
+	<?php include "partials/site-map.php"; ?>
+</header>
