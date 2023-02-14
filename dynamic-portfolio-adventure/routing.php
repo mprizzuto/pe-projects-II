624d2bf7a376aadf47ce262fca33baf50f837ba9
Marco R <mprizzuto88@gmail.com>
2023-02-13 19:10:54 -0500

    update RenderJSON function

--- dynamic-portfolio-adventure/routing.php
+++ dynamic-portfolio-adventure/routing.php
@@ -30,14 +30,39 @@ function renderPageTemplate() {
 	}
 }
 
+// function renderJSON() {
+// 	$pageData = pageData();
+// 	foreach ($pageData["sections"] as $section) {
+// 		foreach($section as $heading => $content) {
+// 			echo <<<THIS
+// 				<h2>$heading</h2> 
+// 				<p>$content</p>
+// 			THIS;
+// 		}
+// 	}
+// }
+
 function renderJSON() {
 	$pageData = pageData();
 	foreach ($pageData["sections"] as $section) {
 		foreach($section as $heading => $content) {
-			echo <<<THIS
+			if ( is_array($content) ) {
+				foreach($content as $key => $value) {
+					echo "<h2>" . $value['title'] ."</h2>";
+					// echo "<pre>";
+					// var_dump($key);
+					// echo "</pre>";
+				}
+			}
+			else {
+				echo <<<THIS
 				<h2>$heading</h2> 
 				<p>$content</p>
 			THIS;
+			// echo var_dump($content);
+			}
+
 		}
 	}
 }
+
