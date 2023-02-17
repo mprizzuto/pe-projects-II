dd8e917a3198a7acd2fda1ae1083a746c7b54ce9
Marco R <mprizzuto88@gmail.com>
2023-02-16 19:50:00 -0500

    Include things grid into things grid template

---
+++ dynamic-portfolio-adventure/partials/things-grid-template.php
@@ -0,0 +1,7 @@
+things-grid-template.php
+
+<section>
+<things-grid>
+	<?php include "things-grid.php";?>
+</things-grid>
+</section>
--- dynamic-portfolio-adventure/partials/things-grid.php
+++ dynamic-portfolio-adventure/partials/things-grid.php
@@ -1,14 +1,10 @@
-<section>
-	<things-grid>
-			<?=$sections["heading"]?>
-
-		<ul>
-			<?php foreach ($sections["items"] as $item) { ?>
-				<li>
-					<h3><?=$item["title"]?></h3>
-					<p><?=$item["copy"]?></p>
-				</li>
-			<?php } ?>
-		</ul>
-	</things-grid>
-</section>
+<?=$sections["heading"]?>
+!!!!
+<ul>
+	<?php foreach ($sections["items"] as $item) { ?>
+		<li>
+			<h3><?=$item["title"]?></h3>
+			<p><?=$item["copy"]?></p>
+		</li>
+	<?php } ?>
+</ul>
