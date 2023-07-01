1396a2d632f2fdf3bce19ef363234686d50b8b52
Marco R <mprizzuto88@gmail.com>
2023-07-01 01:03:44 -0400

    Es modules- Format scripts.js

--- es-modules/scripts/script.js
+++ es-modules/scripts/script.js
@@ -1,40 +1,4 @@
 console.clear();
-// localStorage.clear();
-
-function getPricesAndQuantities() {
-  this.priceQuantityArr = [];
-}
-
-function getCartFromLs() {
-	return JSON.parse(localStorage.getItem("shoppingCart"));
-}
-
-function calculateSubtotal(price, quantity) {
-	return price * quantity;
-}
-
-function generateMessage(price, quantity) {
-	return `your subtotal is ${USDollar.format(calculateSubtotal(price, quantity))} your tax is ${USDollar.format(calculateTax(price, quantity))} your total is ${USDollar.format(calculateTotal(price, quantity))}`;
-}
-
-function calculateTax(price, quantity) {
-	return calculateSubtotal(price, quantity) * TAX_RATE;
-}
-
-function calculateTotal(price, quantity) {
-	return calculateSubtotal(price, quantity) + calculateTax(price, quantity);
-}
-
-getPricesAndQuantities.prototype.addItem = function(item) {
-  this.priceQuantityArr.unshift(item);
-}
-
-const TAX_RATE = 5.5 / 100;
-
-let USDollar = new Intl.NumberFormat("en-US", {
-	style: "currency",
-	currency: "USD",
-});
 
 let shoppingCart = new getPricesAndQuantities();
 
@@ -42,21 +6,37 @@ let shoppingList = document.querySelector("#shopping-item-list");
 
 let shoppingListData = document.querySelector(".shopping-list-prices");
 
+const TAX_RATE = 5.5 / 100;
+
+function getPricesAndQuantities() {
+  this.priceQuantityArr = [];
+}
+
+getPricesAndQuantities.prototype.addItem = function(item) {
+  this.priceQuantityArr.unshift(item);
+}
+
+function getCartFromLs() {
+	return JSON.parse(localStorage.getItem("shoppingCart"));
+}
+
 function calculateOrder(itemPrice,itemQuantity) {
 	shoppingList?.insertAdjacentHTML("afterBegin", `<li>price: ${itemPrice} quantity: ${itemQuantity}</li>`);
 
- // calculateOrder();
  	let subtotal = getCartFromLs().reduce((accumulator, current) => {
  		return accumulator + current.reduce((innerAccumulator, newCurrent) => {
- 		// console.log( accumulator + (calculateSubtotal(newCurrent.itemPrice, newCurrent.itemQuantity)) );
- 		// return innerAccumulator + calculateSubtotal(newCurrent.itemPrice , newCurrent.itemQuantity);
  			return innerAccumulator + (newCurrent.itemPrice * newCurrent.itemQuantity);
  		}, 0);
   }, 0);
-   // console.log(subtotal);
+
   let total = (TAX_RATE * subtotal) + subtotal;
   shoppingListData.textContent =  `your subtotal is ${USDollar.format(subtotal)} the tax is ${USDollar.format(TAX_RATE * subtotal)} the total is ${USDollar.format(total)}`;
 }
+
+let USDollar = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+});
 
 window.addEventListener("click", (event) => {
   event.preventDefault();
@@ -88,16 +68,16 @@ window.addEventListener("click", (event) => {
 	 document.querySelector("input[id$='quantity']").value = "";
   }
 });
-// console.log(getCartFromLs());
+
 window.addEventListener("load", (event) => {
-	// console.log("onLoad, cartLs", getCartFromLs());
 
 	let cartLs = getCartFromLs() ?? null;
+
   cartLs.forEach(item => {
-  	console.log(item)
     item.forEach(subItem => {
       let listItem = `<li>price: ${subItem.itemPrice} quantity: ${subItem.itemQuantity}</li>`;
       shoppingList?.insertAdjacentHTML("beforeEnd", listItem);
+      calculateOrder(subItem.itemPrice, subItem.itemQuantity);
     });
   });
 });
