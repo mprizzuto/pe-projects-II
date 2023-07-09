b3ea75b30bf1c17b3890f994225e88be2945ab62
Marco R <mprizzuto88@gmail.com>
2023-07-09 17:11:27 -0400

    Update files

--- es-modules/components/pages/home.php
+++ es-modules/components/pages/home.php
@@ -1,9 +1,6 @@
 <h1>efp with ES modules</h1>
 <h2 class="exercise-title">shopping cart</h2>
 <p>enter items and quantity into the shopping cart</p>
-<?php $dumbGlobalVar = "pissoffffffff"; 
-
-?>
 
 <?php require_once "./components/forms/shopping-cart-form.php"; ?>
 
