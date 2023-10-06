63e5321a3825f1d5d4aba537512b95fe1adf53ee
marco rizzuto <mprizzuto88@gmail.com>
2023-10-06 03:23:16 -0400

    PHP mvc- Add view files

---
+++ php-mvc/app/views/footer.php
@@ -0,0 +1,12 @@
+				
+				</inner-column>
+			</section>
+		</main>
+		
+		<footer>
+			<inner-column>
+				<h2>footer</h2>
+			</inner-column>
+		</footer>
+	</body>
+</html>
---
+++ php-mvc/public/index.php
@@ -0,0 +1,3 @@
+<?php include "../app/views/header.php"; ?>
+<h2>main title</h2>
+<?php include "../app/views/footer.php"; ?>
